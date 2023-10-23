5d1b68cfc8ee2b50ee6d7a505d57d35f86223c20
Jeyeon Kim <wpdus2694@naver.com>
2023-10-23 10:57:07 +0900

    Initial Commit

---
+++ WebStorage/WebStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebStorageApp.swift
+//  WebStorage
+//
+//  Created by 김제연 on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WebStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
